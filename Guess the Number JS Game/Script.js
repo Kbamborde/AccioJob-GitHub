fce9cc1ecc78464d0c1542c313ab364eab82e9e9
Kbamborde <kunalbamborde00@gmail.com>
2022-01-28 23:59:40 +0530

    Guess the Number Game Project

---
+++ Guess the Number JS Game/Script.js
@@ -0,0 +1,4 @@
+const submit = document.querySelector('#submit');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector(".guesses");
+const remaining = 
