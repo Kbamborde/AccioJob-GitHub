fb2f3681806788ee5107e8e7a944fa8f8a188ad1
Kbamborde <kunalbamborde00@gmail.com>
2022-02-06 20:04:31 +0530

    Final commit

--- Tic Tac Toe/Script.js
+++ Tic Tac Toe/Script.js
@@ -37,6 +37,7 @@ function initializeGame() {
     /* Append game-slots to game div */
     showMessage(`${activePlayer === 0 ? player1 : player2}, you're up!`);
 
+
     for (let i = 1; i <= 9; i++) {
         const div = document.createElement("div");
         div.id = i;
@@ -44,14 +45,22 @@ function initializeGame() {
 
         /* Add Event Listener for Clicking:--> */
         div.addEventListener('click', function () {
-            if (!div.innerText) {
+
+            if (!div.innerText && playGame) {
                 div.innerText = activePlayer === 0 ? 'X' : 'O';
                 const winner = checkWinner();
-                updateActivePlayer();
-                setTimeout(
-                    showMessage(`${activePlayer === 0 ? player1 : player2}, you're up!`),
-                    1000
-                );
+
+                if (!winner) {
+                    updateActivePlayer();
+                    showMessage(`${activePlayer === 0 ? player1 : player2}, you're up!`);
+                }
+                else {
+                    showMessage(`${activePlayer === 0 ? player1 : player2}, congratulations you won!`);
+
+                    setTimeout(()=>{
+                        location.reload();
+                    },3000)
+                }
             }
         });
 
@@ -70,8 +79,30 @@ const winningSequences = [
     [2, 4, 6],
 ];
 
-function checkWinner() {
+function checkWinner(){
+    let winnerBool = false;
 
+    for(let i=0;i<winningSequences.length;i++){
+        const winningCombo = winningSequences[i];
+
+        const cell1 = document.getElementById(winningCombo[0]+1);
+        const cell2 = document.getElementById(winningCombo[1]+1);
+        const cell3 = document.getElementById(winningCombo[2]+1);
+
+        const val1 = cell1.innerText;
+        const val2 = cell2.innerText;
+        const val3 = cell3.innerText;
+
+        if(val1===val2 && val2 === val3 && val1 != ""){
+            winnerBool = true;
+            playGame = false;
+            cell1.style.backgroundColor = "purple";
+            cell2.style.backgroundColor = "purple";
+            cell3.style.backgroundColor = "purple";
+            break;
+        }
+    }
+    return winnerBool;
 }
 
 
